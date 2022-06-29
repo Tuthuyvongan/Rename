4488b68dd3038310f98a68c399d8678a1674ada0
Tuthuyvongan <Tuthuyvongan@gmail.com>
2022-06-29 10:29:30 +0700

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,186 @@
+﻿
+namespace Rename
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.tblMain = new System.Windows.Forms.TableLayoutPanel();
+            this.btSave = new XanderUI.XUIButton();
+            this.lbN = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.txtSave = new System.Windows.Forms.TextBox();
+            this.btLoad = new XanderUI.XUIButton();
+            this.tblMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tblMain
+            // 
+            this.tblMain.BackColor = System.Drawing.Color.White;
+            this.tblMain.ColumnCount = 3;
+            this.tblMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 24.15902F));
+            this.tblMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 55.04588F));
+            this.tblMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20.79511F));
+            this.tblMain.Controls.Add(this.btSave, 2, 1);
+            this.tblMain.Controls.Add(this.lbN, 0, 0);
+            this.tblMain.Controls.Add(this.label1, 0, 1);
+            this.tblMain.Controls.Add(this.txtFile, 1, 0);
+            this.tblMain.Controls.Add(this.txtSave, 1, 1);
+            this.tblMain.Controls.Add(this.btLoad, 2, 0);
+            this.tblMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tblMain.Location = new System.Drawing.Point(0, 0);
+            this.tblMain.Name = "tblMain";
+            this.tblMain.RowCount = 3;
+            this.tblMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12F));
+            this.tblMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12F));
+            this.tblMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 76F));
+            this.tblMain.Size = new System.Drawing.Size(982, 553);
+            this.tblMain.TabIndex = 0;
+            // 
+            // btSave
+            // 
+            this.btSave.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btSave.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(70)))), ((int)(((byte)(75)))));
+            this.btSave.ButtonImage = global::Rename.Properties.Resources.save_file;
+            this.btSave.ButtonStyle = XanderUI.XUIButton.Style.MaterialRounded;
+            this.btSave.ButtonText = "Save File";
+            this.btSave.ClickBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(75)))), ((int)(((byte)(80)))));
+            this.btSave.ClickTextColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(130)))), ((int)(((byte)(140)))));
+            this.btSave.CornerRadius = 5;
+            this.btSave.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btSave.Horizontal_Alignment = System.Drawing.StringAlignment.Center;
+            this.btSave.HoverBackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(80)))), ((int)(((byte)(90)))));
+            this.btSave.HoverTextColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(235)))), ((int)(((byte)(215)))));
+            this.btSave.ImagePosition = XanderUI.XUIButton.imgPosition.Left;
+            this.btSave.Location = new System.Drawing.Point(780, 69);
+            this.btSave.Name = "btSave";
+            this.btSave.Size = new System.Drawing.Size(199, 60);
+            this.btSave.TabIndex = 5;
+            this.btSave.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(195)))), ((int)(((byte)(200)))), ((int)(((byte)(185)))));
+            this.btSave.Vertical_Alignment = System.Drawing.StringAlignment.Center;
+            this.btSave.Click += new System.EventHandler(this.btSave_Click);
+            // 
+            // lbN
+            // 
+            this.lbN.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.lbN.AutoSize = true;
+            this.lbN.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbN.Location = new System.Drawing.Point(91, 21);
+            this.lbN.Name = "lbN";
+            this.lbN.Size = new System.Drawing.Size(143, 23);
+            this.lbN.TabIndex = 0;
+            this.lbN.Text = "File cần rename";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(36, 87);
+            this.label1.Margin = new System.Windows.Forms.Padding(3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(198, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Thư mục chứa kết quả";
+            // 
+            // txtFile
+            // 
+            this.txtFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFile.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFile.Location = new System.Drawing.Point(240, 18);
+            this.txtFile.Margin = new System.Windows.Forms.Padding(3, 3, 8, 3);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.Size = new System.Drawing.Size(529, 30);
+            this.txtFile.TabIndex = 2;
+            // 
+            // txtSave
+            // 
+            this.txtSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSave.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSave.Location = new System.Drawing.Point(240, 84);
+            this.txtSave.Margin = new System.Windows.Forms.Padding(3, 3, 8, 3);
+            this.txtSave.Name = "txtSave";
+            this.txtSave.Size = new System.Drawing.Size(529, 30);
+            this.txtSave.TabIndex = 3;
+            // 
+            // btLoad
+            // 
+            this.btLoad.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btLoad.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(70)))), ((int)(((byte)(75)))));
+            this.btLoad.ButtonImage = global::Rename.Properties.Resources.data_storage;
+            this.btLoad.ButtonStyle = XanderUI.XUIButton.Style.MaterialRounded;
+            this.btLoad.ButtonText = "Load File";
+            this.btLoad.ClickBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(75)))), ((int)(((byte)(80)))));
+            this.btLoad.ClickTextColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(130)))), ((int)(((byte)(140)))));
+            this.btLoad.CornerRadius = 5;
+            this.btLoad.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btLoad.Horizontal_Alignment = System.Drawing.StringAlignment.Center;
+            this.btLoad.HoverBackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(80)))), ((int)(((byte)(90)))));
+            this.btLoad.HoverTextColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(235)))), ((int)(((byte)(215)))));
+            this.btLoad.ImagePosition = XanderUI.XUIButton.imgPosition.Left;
+            this.btLoad.Location = new System.Drawing.Point(780, 3);
+            this.btLoad.Name = "btLoad";
+            this.btLoad.Size = new System.Drawing.Size(199, 60);
+            this.btLoad.TabIndex = 4;
+            this.btLoad.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(195)))), ((int)(((byte)(200)))), ((int)(((byte)(185)))));
+            this.btLoad.Vertical_Alignment = System.Drawing.StringAlignment.Center;
+            this.btLoad.Click += new System.EventHandler(this.btLoad_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(982, 553);
+            this.Controls.Add(this.tblMain);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Rename";
+            this.tblMain.ResumeLayout(false);
+            this.tblMain.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tblMain;
+        private System.Windows.Forms.Label lbN;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.TextBox txtSave;
+        private XanderUI.XUIButton btSave;
+        private XanderUI.XUIButton btLoad;
+    }
+}
+
